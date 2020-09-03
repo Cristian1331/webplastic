041ce4c4eac529fad2b18a989414df5b18d71018
Cristian Andres <sanjic12@outlook.com>
2020-09-02 19:40:11 -0500

    ASP/NET

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPlastic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QuienesSomos()
+        {
+            return View();
+        }
+
+        public ActionResult BlogSingle()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Blog1()
+        {
+            return View();
+        }
+
+        public ActionResult Blog2()
+        {
+            return View();
+        }
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+
+        public ActionResult Catalogo()
+        {
+            return View();
+        }
+
+        public ActionResult Contactanos()
+        {
+            return View();
+        }
+
+        public ActionResult Ingreso()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
