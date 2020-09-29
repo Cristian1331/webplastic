d0c07409010f7caea9788e5a1e631f2036de22a2
arlequin1909 <brayavid19@gmail.com>
2020-09-28 21:27:00 -0500

    cambios de brayan

--- Controllers/AdministratorController.cs
+++ Controllers/AdministratorController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebPlastic.App_Data;
 
 namespace WebPlastic.Controllers
 {
@@ -11,7 +13,11 @@ namespace WebPlastic.Controllers
         // GET: Administrator
         public ActionResult MyProfile()
         {
+            ConnectionDataBase data = new ConnectionDataBase();
+            DataTable dt = data.GetUser("");
+            ViewBag.User = dt.Rows;
             return View();
+
         }
 
         public ActionResult MyCart()
@@ -23,5 +29,28 @@ namespace WebPlastic.Controllers
         {
             return View();
         }
+
+
+        public ActionResult editUser( int id)
+        {
+            ConnectionDataBase.StoreProcediur data = new ConnectionDataBase.StoreProcediur();
+            DataTable dt = data.GetUser(id);
+            ViewBag.Userr= dt.Rows;
+
+            return View();
+        }
+
+
+       
+        public ActionResult UpdateProfile(Models.User model, int id)
+        {
+            model.idUser = id;
+            ConnectionDataBase.StoreProcediur data = new ConnectionDataBase.StoreProcediur();
+            DataTable dt = data.UpdateUser(model);
+            return RedirectToAction("index");
+        }
+
+        
+      
     }
 }
